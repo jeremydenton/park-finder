1a3ef1e0e31a91f960f0b313740f7895e10049bd
jeremydenton <jeremy.denton9@gmail.com>
2019-06-10 14:45:09 -0400

    Authentication working
    Authentication mostly copied from tic-tac-toe project.
    It works but should probably be changed to handlebars.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,11 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events')
 
 $(() => {
-  // your JS code goes here
+  $('#signUp').on('submit', authEvents.onSignUp)
+  $('#signIn').on('submit', authEvents.onSignIn)
+  $('#changePassword').on('submit', authEvents.onChangePassword)
+  $('#signOut').on('submit', authEvents.onSignOut)
 })
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: '<https://shielded-ravine-25383.herokuapp.com/>',
   development: 'http://localhost:4741'
 }
 
